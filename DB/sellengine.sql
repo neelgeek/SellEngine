a09438ea1d9ebc348bdd1fc9664b8c7ed3fe95a4
neelgeek <neelbhave91@gmail.com>
2018-04-10 19:52:08 +0530

    Updated db with products

--- DB/sellengine.sql
+++ DB/sellengine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2018 at 04:14 PM
+-- Generation Time: Apr 10, 2018 at 04:21 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -21,6 +21,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `sellengine`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `prod_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `price` int(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `descp` varchar(500) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`prod_id`, `title`, `type`, `price`, `city`, `descp`, `user_id`) VALUES
+(1, 'Test', 'Electronics', 1000, 'Thane', 'Hello World !', 1);
 
 -- --------------------------------------------------------
 
@@ -46,6 +69,12 @@ INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -54,6 +83,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
